be9448778f026a73d7a8c6716940a3e14ec41e04
Sergii Prostov <spro77@gmail.com>
2025-10-02 21:34:13 -0600

    Categories and Accessories added

--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -15,8 +15,22 @@ app.MapGet("/api/productlist", () =>
 {
     return new[]
     {
-        new { Id = 1, Name = "Laptop", Price = 1200.50, Stock = 25 },
-        new { Id = 2, Name = "Headphones", Price = 50.00, Stock = 100 }
+        new
+        {
+            Id = 1,
+            Name = "Laptop",
+            Price = 1200.50,
+            Stock = 25,
+            Category = new { Id = 101, Name = "Electronics" }
+        },
+        new
+        {
+            Id = 2,
+            Name = "Headphones",
+            Price = 50.00,
+            Stock = 100,
+            Category = new { Id = 102, Name = "Accessories" }
+        }
     };
 });
 
